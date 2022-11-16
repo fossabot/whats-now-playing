96e949e4af9804aa5a75a13b92d095691bf72dc2
Allen Wittenauer <aw@apache.org>
2022-11-16 08:54:35 -0800

    1

--- nowplaying/inputs/stagelinq.py
+++ nowplaying/inputs/stagelinq.py
@@ -172,7 +172,7 @@ class Plugin(InputPlugin):
     def __init__(self, config=None, qsettings=None):
         super().__init__(config=config, qsettings=qsettings)
         self.mixmode = "newest"
-        self.event_handler = None
+        self.driver = None
         self.qwidget = None
         self._reset_meta()
 
@@ -184,88 +184,11 @@ class Plugin(InputPlugin):
         ''' set up a custom watch on the m3u dir so meta info
             can update on change'''
 
-        self.driver = PyStagelinQDriver(self.incoming)
-        if not self.m3udir or self.m3udir != m3udir:
-            await self.stop()
+        self.driver = PyStagelinQDriver(self.update_track)
 
-        if self.observer:
-            return
 
-        self.m3udir = m3udir
-        if not self.m3udir:
-            logging.error('M3U Directory Path does not exist: %s', self.m3udir)
-            return
-
-        logging.info('Watching for changes on %s', self.m3udir)
-        #if len(os.listdir(self.m3udir)) < 1:
-        #    pathlib.Path(os.path.join(self.m3udir, 'empty.m3u')).touch()
-
-    @staticmethod
-    def _read_track_default(filename):
-        content = None
-        with open(filename, 'rb') as m3ufh:
-            while True:
-                newline = m3ufh.readline()
-                if not newline:
-                    break
-                newline = newline.rstrip()
-                if not newline or newline[0] == '#':
-                    continue
-                content = newline
-        return content
-
-    def _read_track(self, event):
-
-        if event.is_directory:
-            return
-
-        filename = event.src_path
-        logging.debug('event type: %s, syn: %s, path: %s', event.event_type,
-                      event.is_synthetic, filename)
-
-        # file is empty so ignore it
-        if os.stat(filename).st_size == 0:
-            logging.debug('%s is empty, ignoring for now.', filename)
-            self._reset_meta()
-            return
-
-        content = self._read_track_default(filename)
-
-        logging.debug('attempting to parse \'%s\' with various encodings',
-                      content)
-
-        if not content:
-            self._reset_meta()
-            return
-
-        found = None
-        for encoding in ['utf-8', 'ascii', 'cp1252', 'utf-16']:
-            try:
-                location = content.decode(encoding)
-            except UnicodeDecodeError:
-                logging.debug('Definitely not %s', encoding)
-                continue
-            location = location.replace('file://', '')
-            location = nowplaying.utils.songpathsubst(self.config, location)
-            if os.path.exists(location):
-                found = location
-                break
-
-            dirpath = os.path.dirname(filename)
-            attempt2 = os.path.join(dirpath, location)
-            attempt2 = nowplaying.utils.songpathsubst(self.config, attempt2)
-            if os.path.exists(attempt2):
-                found = attempt2
-                break
-
-        if not found:
-            logging.error('Cannot find or decode %s', content)
-            self._reset_meta()
-            return
-
-        logging.debug('Used %s and found %s', encoding, found)
-        newmeta = {'filename': found}
-        Plugin.metadata = newmeta
+    def update_track(self):
+        pass
 
     async def start(self):
         ''' setup the watcher to run in a separate thread '''
@@ -296,10 +219,9 @@ class Plugin(InputPlugin):
     async def stop(self):
         ''' stop the m3u plugin '''
         self._reset_meta()
-        if self.observer:
-            self.observer.stop()
-            self.observer.join()
-            self.observer = None
+        if self.driver:
+            self.driver.stop()
+            self.driver = None
 
     def on_m3u_dir_button(self):
         ''' filename button clicked action'''
